30cda588fb92534a5d58f983ff441bad5550e6f7
Maximilian von Gaisberg <git@vongaisberg.de>
2025-12-19 15:31:06 +0100

    Update dependencies for RP2350, switch to nightly Rust toolchain

--- build.rs
+++ build.rs
@@ -1,5 +1,11 @@
+//! Build script for RP2350 ArtNet Node
+
 fn main() {
+    // Link arguments for RP2350
     println!("cargo:rustc-link-arg-bins=--nmagic");
     println!("cargo:rustc-link-arg-bins=-Tlink.x");
     println!("cargo:rustc-link-arg-bins=-Tdefmt.x");
+    
+    // Re-run if memory.x changes
+    println!("cargo:rerun-if-changed=memory.x");
 }
